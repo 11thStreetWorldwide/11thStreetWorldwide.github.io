76c799112cfe2b88fb022465bf6d7bb8f8eb6328
rakane <rakane@syr.edu>
2019-01-25 09:53:50 -0500

    Added fade animation for lineup page

---
+++ js/lineupfade.js
@@ -0,0 +1,24 @@
+/* eslint-disable func-names */
+$(() => {
+  // $(document).ready shorthand
+  $('#member-one').fadeIn('slow');
+  $('#member-two').fadeIn('slow');
+  $('#member-three').fadeIn('slow');
+  $('#member-four').fadeIn('slow');
+});
+
+$(document).ready(() => {
+  /* Every time the window is scrolled ... */
+  $(window).scroll(() => {
+    /* Check the location of each desired element */
+    $('.member-container').each(function(i) {
+      let topOfObject = $(this).position().top;
+      let bottomOfWindow = $(window).scrollTop() + $(window).height();
+
+      /* If the object is visible in window */
+      if (bottomOfWindow > topOfObject) {
+        $(this).animate({ opacity: '1' }, 1500);
+      }
+    });
+  });
+});
